2b866122ef567e6a0d1838cc4d6dd7e0e65cb60a
RamziHkiri <6257@holbertonstudents.com>
2023-09-22 16:35:53 +0200

    3-starwars_title.js

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { error } = require('console');
+const request = require('request');
+
+const id = process.argv[2];
+
+request('https://swapi-api.hbtn.io/api/films/' + id, (error, response, body) =>{
+    if (error) {
+        console.log(error);
+      } else {
+        console.log(JSON.parse(body).title);
+      }
+});
