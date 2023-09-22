2aa88cd346a1bf3072bf26f096c02d34a1a935bc
RamziHkiri <6257@holbertonstudents.com>
2023-09-22 15:22:18 +0200

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const f = require('fs');
 const argv = process.argv[2];
 
-f.readFile (argv[2], 'utf-8', function (error, data) {
+f.readFile (argv, 'utf-8', function (error, data) {
  if (error) {
   console.error(error);
   return;
